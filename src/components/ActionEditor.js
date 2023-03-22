26b01461eca67f8f6a2c44b8e4f437c5a73b06ff
Caleb Favela <cfc3.1415@gmail.com>
2023-03-22 21:32:21 +0000

    Correct ref use in previous commit

--- src/components/ActionEditor.js
+++ src/components/ActionEditor.js
@@ -1,47 +1,57 @@
-import React from 'react';
+import React from "react";
 
-function JsonEditor({object, deleteObject, setObject}) {
+function ActionEditor({ object, deleteObject, setObject }) {
+  let urlRef = React.createRef();
+  let iconRef = React.createRef();
+let nameRef = React.createRef();
 
-    let urlRef = React.createRef();
-    let iconRef = React.createRef();
+  const deleteSelf = () => {
+    deleteObject(object);
+  };
 
-    let objectText = JSON.stringify(object, null, " ");
-    
+  const saveSelf = (e) => {
+    setObject({ ...object, templateLink: urlRef.current.value, icon: iconRef.current.value, name: nameRef.current.value });
+    e.preventDefault();
+  };
 
-    const deleteSelf = () => {
-        deleteObject(object);
-    }
-
-    const saveSelf = (e) => {
-        setObject({...object ,templateLink:urlRef, icon:iconRef});
-        e.preventDefault();
-    }
-
-    return (
-        <div className="card cyan lighten-5">
-            <div className="card-content">
-                <form onSubmit={saveSelf} >
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <label htmlFor="url">{"URL and Icon:"}</label>
-                            <textarea id="url" ref={urlRef} defaultValue={object.templateLink} className="materialize-textarea"></textarea>
-                            <textarea id="icon" ref={iconRef} defaultValue={object.icon} className="materialize-textarea"></textarea>
-                        </div>
-                    </div>
-                    <button className="btn waves-effect" type="submit">
-                        Save <i className="material-icons right">save</i>
-                    </button>
-                    
-                    <button className="btn waves-effect right" onClick={deleteSelf}>
-                        Delete <i className="material-icons right">delete_forever</i>
-                    </button>
-                </form>
-
-                      
+  return (
+    <div className="card cyan lighten-5">
+      <div className="card-content">
+        <form onSubmit={saveSelf}>
+          <div className="row">
+            <div className="input-field col s12">
+              <label htmlFor="name">{"Name, URL, and Icon:"}</label>
+              <textarea
+                id="name"
+                ref={nameRef}
+                defaultValue={object.name}
+                className="materialize-textarea"
+              ></textarea>
+              <textarea
+                id="url"
+                ref={urlRef}
+                defaultValue={object.templateLink}
+                className="materialize-textarea"
+              ></textarea>
+              <textarea
+                id="icon"
+                ref={iconRef}
+                defaultValue={object.icon}
+                className="materialize-textarea"
+              ></textarea>
             </div>
-        </div>
-    );
+          </div>
+          <button className="btn waves-effect" type="submit">
+            Save <i className="material-icons right">save</i>
+          </button>
+
+          <button className="btn waves-effect right" onClick={deleteSelf}>
+            Delete <i className="material-icons right">delete_forever</i>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default JsonEditor;
-
+export default ActionEditor;
